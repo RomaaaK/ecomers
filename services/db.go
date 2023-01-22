c1dde48f1ea461dac2e0fdefdaae8b5eae45ab65
RomaaaK <zimpsonzya@gmail.com>
2023-01-22 17:58:14 +0300

    delete comments

--- services/db.go
+++ services/db.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"example/ecomers/models"
-	"fmt"
 	"log"
 	"os"
 
@@ -16,8 +15,6 @@ var DB *gorm.DB
 func InitDataBase() {
 	db_user := os.Getenv("DB_USER")
 	db_pass := os.Getenv("DB_PASS")
-
-	fmt.Println(db_user, db_pass, "-----------------------------------")
 
 	dsn := db_user + ":" + db_pass + "@tcp(localhost:3306)/ecomers?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
